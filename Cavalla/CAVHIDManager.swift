48eeabed2739ce07859dbc40a711f872425390fe
Ken Heglund <github@orderedbytes.com>
2016-11-15 07:51:13 -0800

    Constructs CAVHIDDevice objects on a background queue.

--- Cavalla/CAVHIDManager.swift
+++ Cavalla/CAVHIDManager.swift
@@ -69,13 +69,19 @@ private func CAVHIDManagerDeviceAttachedHandler( context: UnsafeMutableRawPointe
         print( "Device attached: \(product) (\(manufacturer)) \(vendorID):\(productID) \(usagePage):\(usage) [\(result)]" )
     }
     
-    IOHIDDeviceRegisterRemovalCallback( hidDeviceRef, CAVHIDManagerDeviceRemovedHandler, context )
-    
-    guard let device = CAVHIDDevice( withHIDDeviceRef: hidDeviceRef ) else { return }
-    
-    guard let context = context else { return }
-    let manager = Unmanaged<CAVHIDManager>.fromOpaque( context ).takeUnretainedValue()
-    manager.mutableSetValue( forKey: CAVHIDManagerDevicesKey ).add( device )
+    DispatchQueue.global( priority: DispatchQueue.GlobalQueuePriority.default ).async {
+        
+        IOHIDDeviceRegisterRemovalCallback( hidDeviceRef, CAVHIDManagerDeviceRemovedHandler, context )
+        
+        guard let device = CAVHIDDevice( withHIDDeviceRef: hidDeviceRef ) else { return }
+        
+        guard let context = context else { return }
+        let manager = Unmanaged<CAVHIDManager>.fromOpaque( context ).takeUnretainedValue()
+        
+        DispatchQueue.main.async {
+            manager.mutableSetValue( forKey: CAVHIDManagerDevicesKey ).add( device )
+        }
+    }
 }
 
 /*==========================================================================*/
